07a284e7b90b6bf0efd5798a3e6b5a84f5dfed96
daseul.ju <daseul.ju@mobyus.com>
2026-03-12 18:24:31 +0900

    force: staging deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.0.34:9090",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
