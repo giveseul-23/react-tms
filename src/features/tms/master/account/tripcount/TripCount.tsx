245b1d68e08672a7b3f714fca3eedda2f27afe93
dasol.kim <dasol.kim@mobyus.com>
2026-05-15 13:34:48 +0900

    [구간별인정회전수관리] 화면 추가

---
+++ src/features/tms/master/account/tripcount/TripCount.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import DataGrid from "@/app/components/grid/DataGrid";
+
+import { useTripCountModel } from "./TripCountModel";
+import { useTripCountController } from "./TripCountController";
+import { MAIN_COLUMN_DEFS } from "./TripCountColumns";
+import { GridOnlyPage } from "@/app/components/layout/presets/GridOnlyPage";
+
+export const MENU_CODE = "MENU_TRIPCNT_MGMT";
+
+export default function TripCount() {
+  const model = useTripCountModel(MENU_CODE);
+  const ctrl = useTripCountController({ model });
+
+  return (
+    <GridOnlyPage
+      menuCode={MENU_CODE}
+      searchProps={{
+        fetchFn: ctrl.fetchList,
+        onSearch: ctrl.handleSearch,
+        searchRef: model.searchRef,
+        filtersRef: model.filtersRef,
+        pageSize: model.pageSize,
+      }}
+      grid={
+        <DataGrid
+          {...model.bind("main")}
+          columnDefs={MAIN_COLUMN_DEFS}
+          actions={ctrl.mainActions}
+        />
+      }
+    />
+  );
+}
