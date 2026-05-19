338023648a56824b34e2f2b045b797ae7a452add
hyewon.yeon <hyewon.yeon@mobyus.com>
2026-05-19 15:39:43 +0900

    [선호운송협력사] import 수정

--- src/features/tms/master/account/prefcarrier/PreferredCarrierController.tsx
+++ src/features/tms/master/account/prefcarrier/PreferredCarrierController.tsx
@@ -4,7 +4,7 @@ import {
   makeAddAction,
   makeSaveAction,
   makeExcelGroupAction,
-} from "@/app/components/grid/commonActions";
+} from "@/app/components/grid/actions/commonActions";
 import { preferredCarrierApi as api } from "./PreferredCarrierApi";
 import { MAIN_COLUMN_DEFS } from "./PreferredCarrierColumns";
 import type { ActionItem } from "@/app/components/ui/GridActionsBar";
